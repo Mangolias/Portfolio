2733366be0997faad9d9b16838864519597a9637
Laura Laamanen <laura.laamanen@fluidogroup.onmicrosoft.com>
2025-02-14 13:48:58 +0200

    initial commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+document.getElementById("project-one").addEventListener("mouseover", function() {
+    document.getElementById("projects-page").style.backgroundImage = "url(src/images/project-one.png)";
+  }, false);
+  document.getElementById("project-one").addEventListener("mouseout", function() {
+     document.getElementById("projects-page").style.backgroundImage = "";
+  }, false);
+
+  document.getElementById("project-two").addEventListener("mouseover", function() {
+    document.getElementById("projects-page").style.backgroundImage = "url(src/images/project-two.png)";
+  }, false);
+  document.getElementById("project-two").addEventListener("mouseout", function() {
+     document.getElementById("projects-page").style.backgroundImage = "";
+  }, false);
+
+  document.getElementById("project-three").addEventListener("mouseover", function() {
+    document.getElementById("projects-page").style.backgroundImage = "url(src/images/project-three.png)";
+  }, false);
+  document.getElementById("project-three").addEventListener("mouseout", function() {
+     document.getElementById("projects-page").style.backgroundImage = "";
+  }, false);
+
+  document.getElementById("project-four").addEventListener("mouseover", function() {
+    document.getElementById("projects-page").style.backgroundImage = "url(src/images/project-four.gif)";
+  }, false);
+  document.getElementById("project-four").addEventListener("mouseout", function() {
+     document.getElementById("projects-page").style.backgroundImage = "";
+  }, false);
